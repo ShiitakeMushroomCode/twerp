f11b2898940f1fc72f0ae1db342208708e620f9f
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-09-30 01:04:48 +0900

    .

--- util/getSample.ts
+++ util/getSample.ts
@@ -1,9 +1,15 @@
 import { executeQuery } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
-    const data = await executeQuery('SELECT * from table');
+    const { searchParams } = new URL(request.url);
+    // 사용자 입력값 가져오기
+    const someParam = searchParams.get('param');
+
+    // SQL 쿼리에서 파라미터 사용
+    const data = await executeQuery('SELECT * FROM table WHERE column = ?', [someParam]);
+
     return NextResponse.json({
       data,
     });
