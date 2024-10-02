87427f8bb53ca17410b45bd2fe6ac7b0125eabe1
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-10-03 07:25:41 +0900

    .

---
+++ app/(회계)/(예산)/Budget-add/page.tsx
@@ -0,0 +1,6 @@
+export const metadata = {
+  title: '예산 추가',
+};
+export default async function Page() {
+  return <div>예산 추가하는 곳</div>;
+}
---
+++ app/(회계)/(예산)/Budget-list/page.tsx
@@ -0,0 +1,6 @@
+export const metadata = {
+  title: '예산 목록',
+};
+export default async function Page() {
+  return <div>예산 확인하는 곳</div>;
+}
