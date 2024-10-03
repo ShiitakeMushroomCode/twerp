6493683bc52c339d19d4923cc041e999dcad62cd
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-10-04 04:24:16 +0900

    .

--- util/token.ts
+++ util/token.ts
@@ -1,8 +1,6 @@
-import { auth } from '@/auth';
 import { executeQuery } from '@/lib/db';
 
-export async function MatchRefToken() {
-  const session = await auth();
+export async function MatchRefToken({ session }) {
   if (session) {
     const data = await executeQuery(`SELECT ref_token FROM employee WHERE employee_id = unhex(?)`, [session.user.id]);
     return true;
