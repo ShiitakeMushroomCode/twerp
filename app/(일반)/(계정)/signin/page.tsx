93bfb31e518b9803dcc17dffd89b49b7e016d655
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-10-04 21:55:18 +0900

    .

--- app/(일반)/(계정)/signin/page.tsx
+++ app/(일반)/(계정)/signin/page.tsx
@@ -1,4 +1,4 @@
-import styles from '@/styles/SignInPage.module.css';
+import SinginCom from '@/components/Auth/signin';
 
 export const metadata = {
   title: '로그인',
@@ -10,43 +10,5 @@ export default async function SignInPage() {
     console.log(formData);
   }
 
-  return (
-    <form action={signin} className={styles.formContainer}>
-      <div className={styles.formGroup}>
-        <label htmlFor="phone_number" className={styles.label}>
-          전화번호:
-        </label>
-        <input
-          type="text"
-          id="phone_number"
-          name="phone_number"
-          required
-          placeholder="전화번호 입력 (숫자만 11자리)"
-          className={styles.input}
-          pattern="\d{11}"
-          title="전화번호는 11자리 숫자여야 합니다. (010####-####)"
-          autoComplete="off"
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="password" className={styles.label}>
-          비밀번호:
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          required
-          className={styles.input}
-          placeholder="비밀번호 입력 (영문, 숫자, 특수문자 선택)"
-          pattern="(?=.*\d)(?=.*[a-zA-Z])[A-Za-z\d@$!%*?&]{8,}"
-          title="비밀번호는 최소 8자 이상이어야 하며, 영문 대문자 또는 소문자와 숫자를 포함해야 합니다. 특수문자는 선택 사항입니다."
-          autoComplete="off"
-        />
-      </div>
-      <button type="submit" className={styles.button}>
-        로그인
-      </button>
-    </form>
-  );
+  return <SinginCom signin={signin} />;
 }
--- util/token.ts
+++ util/token.ts
@@ -1,10 +1,30 @@
 import { executeQuery } from '@/lib/db';
+import { SignJWT } from 'jose';
 
-export async function MatchRefToken({ session }) {
-  if (session) {
-    const data = await executeQuery(`SELECT ref_token FROM employee WHERE employee_id = unhex(?)`, [session.user.id]);
-    return true;
-  } else {
-    return false;
-  }
+export async function StoreRefreshToken({ userId, refreshToken }) {
+  const query = `
+    UPDATE employee
+    SET ref_token = ?
+    WHERE user_id = unhex(?);
+  `;
+  const result = await executeQuery(query, [refreshToken, userId]);
+  return result;
+}
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function generateAccessToken(userId: string, role: string) {
+  return new SignJWT({ userId, role })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime(process.env.JWT_EXPIRATION) // Access token은 15분동안 지속
+    .sign(secret);
+}
+
+export async function generateRefreshToken(userId: string) {
+  return new SignJWT({ userId })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime(process.env.REFRESH_TOKEN_EXPIRATION) // Refresh token 30일동안 지속
+    .sign(secret);
 }
