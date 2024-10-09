807a81ad167fad3330fe4a6ddcf52650184ff65f
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-10-10 02:08:53 +0900

    로긴 최적화

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -14,16 +14,6 @@ export async function POST(request: NextRequest) {
     const data = (await StoreAndGetUserData(id, refreshToken))[0];
 
     const innerData: ACT = await getInnerData(data);
-    // {
-    //   userId: await data?.phone_number, // 휴대전화 번호
-    //   department: await data?.department, // 부서명
-    //   name: await data?.name, // 이름
-    //   tellNumber: await data?.tellNumber, // 전화번호
-    //   position: await data?.position, // 직급
-    //   email: await data?.email, // 이메일
-    //   hireDate: await data?.hire_date.toISOString().split('T')[0], // 입사일
-    //   status: await data?.status, // 현상태
-    // };
 
     const accessToken = await generateAccessToken(innerData);
     const response = NextResponse.json({
