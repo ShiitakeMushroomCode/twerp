a49fd5a9429a29c2552886f99d1d7d0fd38eb348
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-10-10 15:38:07 +0900

    not-found 로깅

---
+++ app/api/log/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      const time = new Date(timestamp).toLocaleString('ko-KR', {
+        timeZone: 'Asia/Seoul',
+      });
+      return `${time} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [new winston.transports.Console(), new winston.transports.File({ filename: 'not-found.log' })],
+});
+
+export async function POST(request: NextRequest) {
+  const { pathname } = await request.json();
+
+  // 클라이언트의 IP 주소 가져오기
+  let ip =
+    request.headers.get('x-forwarded-for') ||
+    request.headers.get('x-real-ip') ||
+    request.headers.get('remote-addr') ||
+    'Unknown IP';
+  ip = ip.replace(/^::ffff:/, '');
+
+  logger.info(`${ip}에서 ${pathname} 경로에 접근 시도`);
+
+  return NextResponse.json({ message: 'Logged' });
+}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,24 @@
-// import { getIpAddress } from '@/util/getIpAddress';
-import { Metadata } from 'next';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'Not found',
-};
+import { usePathname } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+
 export default function NotFound() {
-  //console.log(getIpAddress());
+  const pathname = usePathname();
+  const hasLogged = useRef(false);
+
+  useEffect(() => {
+    if (hasLogged.current) return;
+    hasLogged.current = true;
+
+    fetch('/api/log', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ pathname }),
+    });
+  }, [pathname]);
+
   return <div>존재하지 않는 페이지입니다.</div>;
 }
