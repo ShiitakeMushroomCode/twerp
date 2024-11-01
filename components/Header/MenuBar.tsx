7507b6ce76517de1faf05a14988b840941cfc566
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-11-02 00:43:58 +0900

    여기서 만족해야겠다

--- components/Header/MenuBar.tsx
+++ components/Header/MenuBar.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
+import { useCallback } from 'react';
 import styles from './MenuBar.module.css';
 
 // 메뉴 항목 정의
@@ -11,8 +11,7 @@ const erpMenuItems = [
   { title: '매출 정보', href: '/sales-list' },
   { title: '매입 정보', href: '/purchase-list' },
   { title: '제품 목록', href: '/items-list' },
-  // { title: '재고', href: '/inventory-list' },
-  // { title: '세금계산서', href: '/invoice-list' },
+  // 추가 메뉴 항목이 필요하면 여기에 추가하세요
 ];
 
 export default function MenuBar() {
@@ -20,33 +19,35 @@ export default function MenuBar() {
   const router = useRouter(); // 라우터 인스턴스를 가져옴
 
   // 리프레시가 필요한 경로 목록
-  const ROUTES = ['/sales-list', '/client-list', '/purchase-list', '/items-list'];
+  const isListRoute = (path) => path.endsWith('-list');
 
-  const handleClick = (href: string) => (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    // 클릭한 링크의 href가 현재 경로와 일치하고, 지정된 ROUTES에 포함되는 경우
-    if (href === pathname && ROUTES.includes(pathname)) {
-      router.replace(pathname);
-    }
-  };
+  const handleClick = useCallback(
+    (href) => (e) => {
+      // 클릭한 링크의 href가 현재 경로와 일치하고, 경로가 'list'로 끝나는 경우
+      if (href === pathname && isListRoute(pathname)) {
+        e.preventDefault();
+        window.location.reload();
+      }
+    },
+    [pathname, router]
+  );
 
   return (
-    <AppBar position="static" color="default" className={styles.menu}>
-      <Toolbar className={styles.toolbar}>
+    <nav className={styles.menu}>
+      <div className={styles.toolbar}>
         <div className={styles.menuContent}>
           {erpMenuItems.map((item, index) => (
-            <Button
+            <Link
               key={index}
-              color="inherit"
-              component={Link}
               href={item.href}
+              className={`${styles.menuItem} ${pathname === item.href ? styles.active : ''}`}
               onClick={handleClick(item.href)}
-              className={styles.menuItem}
             >
-              <Typography variant="body1">{item.title}</Typography>
-            </Button>
+              {item.title}
+            </Link>
           ))}
         </div>
-      </Toolbar>
-    </AppBar>
+      </div>
+    </nav>
   );
 }
