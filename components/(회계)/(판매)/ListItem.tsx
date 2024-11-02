2363c2ce86ed50bad068e6e98ef814005ec8feec
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-11-03 03:06:46 +0900

    .

--- components/(회계)/(판매)/ListItem.tsx
+++ components/(회계)/(판매)/ListItem.tsx
@@ -260,7 +260,7 @@ export default function SalesListItem({
             <tr>
               <th>복사</th>
               <th onClick={() => handleSort('sale_date')}>
-                <span className={styles.headerCell}>
+                <span className={styles.headerCell} title="거래일자">
                   <span className={styles.fakeLabel}>▲</span>
                   거래일자
                   <span className={styles.sortArrow}>
@@ -269,7 +269,7 @@ export default function SalesListItem({
                 </span>
               </th>
               <th onClick={() => handleSort('company_name')}>
-                <span className={styles.headerCell}>
+                <span className={styles.headerCell} title="거래처명">
                   <span className={styles.fakeLabel}>▲</span>
                   거래처명
                   <span className={styles.sortArrow}>
@@ -278,7 +278,7 @@ export default function SalesListItem({
                 </span>
               </th>
               <th onClick={() => handleSort('item_names')}>
-                <span className={styles.headerCell}>
+                <span className={styles.headerCell} title="품목명">
                   <span className={styles.fakeLabel}>▲</span>
                   품목명
                   <span className={styles.sortArrow}>
@@ -287,7 +287,7 @@ export default function SalesListItem({
                 </span>
               </th>
               <th onClick={() => handleSort('total_amount')}>
-                <span className={styles.headerCell}>
+                <span className={styles.headerCell} title="금액 합계">
                   <span className={styles.fakeLabel}>▲</span>
                   금액 합계
                   <span className={styles.sortArrow}>
@@ -295,8 +295,8 @@ export default function SalesListItem({
                   </span>
                 </span>
               </th>
-              <th>인쇄</th>
-              <th>회계 반영</th>
+              <th title="거래명세표 인쇄">거래명세표</th>
+              <th title="회계 반영 여부">회계 반영</th>
             </tr>
           </thead>
           <tbody>
