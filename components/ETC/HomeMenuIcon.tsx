f2f0ab2437ef31b6506575e4583f6d250034de60
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-11-08 00:04:33 +0900

    흠

--- components/ETC/HomeMenuIcon.tsx
+++ components/ETC/HomeMenuIcon.tsx
@@ -5,7 +5,7 @@ import styles from './HomeMenuIcon.module.css';
 export default function HomeMenuIcon({ href, src, alt, title }) {
   return (
     <Link href={href} className={styles.iconLink}>
-      <Image width={150} height={150} src={src} alt={alt}></Image>
+      <Image width={150} height={150} src={src} alt={alt} priority></Image>
       <p className={styles.iconTitle}>{title}</p>
     </Link>
   );
