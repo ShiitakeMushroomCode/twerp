8a7f232d2edc818cc2931722089ab7a0128a974d
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-11-09 15:42:50 +0900

    .

--- components/(회계)/(판매)/(Print)/SalesPrintForm.tsx
+++ components/(회계)/(판매)/(Print)/SalesPrintForm.tsx
@@ -1,11 +1,8 @@
-/** @jsxImportSource @emotion/react */
-'use client';
-import { css } from '@emotion/react';
 import { format } from 'date-fns';
-import React from 'react';
+import styles from './SalesPrintForm.module.css';
 
 export interface CompanyResult {
-  company_id: string;
+  company_id: Buffer;
   business_number: string;
   company_name: string;
   representative_name: string;
@@ -21,9 +18,9 @@ export interface CompanyResult {
 }
 
 export interface SalesResult {
-  sales_id: string;
-  company_id: string;
-  client_id: string | null;
+  sales_id: Buffer;
+  company_id: Buffer;
+  client_id: Buffer | null;
   client_name: string | null;
   client_address: string | null;
   client_tel: string | null;
@@ -38,8 +35,8 @@ export interface SalesResult {
 }
 
 export interface SalesItemResult {
-  sales_item_id: string;
-  sales_id: string;
+  sales_item_id: Buffer;
+  sales_id: Buffer;
   product_id: string | null;
   product_name: string;
   standard: string | null;
@@ -56,197 +53,31 @@ export interface SalesPrintFormData {
   salesItemsResult: SalesItemResult[];
 }
 
+interface Props {
+  salesFormData: SalesPrintFormData;
+}
 
-const containerStyle = css`
-  display: flex;
-  flex-direction: column;
-  padding: 0;
-  width: 100%;
-  max-width: 1078px;
-  margin: 0 auto;
-  background-color: #ffffff;
-  font-size: small;
-  width: auto;
-`;
-
-const headerStyle = css`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid black;
-  padding-bottom: 5px;
-  margin-bottom: 16px;
-`;
-
-const titleStyle = css`
-  margin-left: 1rem;
-  font-size: 28px;
-  font-weight: bold;
-  margin-bottom: 10px;
-`;
-
-const subTitleStyle = css`
-  font-size: 20px;
-  font-weight: bold;
-  margin: 0;
-  padding: 8px;
-  text-align: center;
-  background-color: #f0f0f0;
-`;
-
-const infoTableStyle = css`
-  width: 100%;
-  border-collapse: collapse;
-  margin-bottom: 1rem;
-  border: 1px solid black;
-
-  th,
-  td {
-    border: 1px solid black;
-    padding: 8px;
-    text-align: center;
-  }
-
-  th {
-    background-color: #f0f0f0;
-    font-weight: bold;
-  }
-
-  .label {
-    width: 20%;
-    font-weight: bold;
-    text-align: center;
-  }
-
-  .value {
-    width: 30%;
-    text-align: left;
-  }
-
-  .LLabel {
-    padding-right: 1rem;
-    font-weight: bold;
-  }
-
-  .longLabel {
-    width: 30%;
-    font-weight: bold;
-  }
-
-  .shortLabel {
-    width: 10%;
-    font-weight: bold;
-  }
-
-  .footerLabel {
-    font-weight: bold;
-    background-color: #f9f9f9;
-    text-align: right;
-    border-top: 2px solid black;
-  }
-
-  .footerValue {
-    font-weight: bold;
-    background-color: #f9f9f9;
-    text-align: right;
-    border-top: 2px solid black;
-  }
-`;
-
-const transactionTableStyle = css`
-  width: 100%;
-  border-collapse: collapse;
-  margin-bottom: 1rem;
-  border: 1px solid black;
-
-  th,
-  td {
-    border: 1px solid black;
-    padding: 8px;
-    text-align: center;
-  }
-
-  th {
-    background-color: #f0f0f0;
-    font-weight: bold;
-  }
-
-  .resultTransactionTable tr td {
-    padding-left: 0;
-    padding-right: 0;
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  .resultTransactionTable tr td:nth-of-type(even) {
-    width: 12%;
-  }
-
-  .resultTransactionTable tr td:nth-of-type(odd) {
-    background-color: #f0f0f0;
-    width: 8%;
-  }
-
-  .resultTransactionTable tr td.header {
-    background-color: #f0f0f0;
-    font-weight: bold;
-  }
-
-  .resultTransactionTable tr td.value {
-    text-align: right;
-  }
-`;
-
-const resultTransactionTableStyle = css`
-  ${transactionTableStyle}
-`;
-
-// 새로운 스타일 추가
-const accountNumberStyle = css`
-  width: 30%;
-  font-weight: bold;
-  text-align: center;
-`;
-
-const smallTextStyle = css`
-  font-size: 12px; /* 원하는 크기로 조정 */
-  font-weight: bold;
-  background-color: #f9f9f9;
-  text-align: center;
-  border-top: 2px solid black;
-`;
-
-const SalesPrintFormComponent: React.FC<any> = ({ salesFormData }) => {
+export default function SalesPrintFormComponent({ salesFormData }: Props) {
   const { companyResult, salesResult, salesItemsResult } = salesFormData;
   let totalP = 0;
   let totalPrice = 0;
   let totalSub_price = 0;
   let totalQuantity = 0;
-
-  salesItemsResult.forEach((element:any) => {
+  salesItemsResult.forEach((element) => {
     totalP += element.price;
     totalPrice += element.price * element.quantity;
     totalSub_price += element.sub_price;
     totalQuantity += element.quantity;
   });
-
   let total = totalPrice + totalSub_price;
-
   return (
-    <div css={containerStyle}>
-      <div css={headerStyle}>
-        <h1 css={titleStyle}>거래명세서</h1>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>거래명세서</h1>
         <table>
           <tbody>
             <tr>
-              <td
-                css={css`
-                  padding-right: 1rem;
-                  font-weight: bold;
-                `}
-              >
-                거래일자
-              </td>
+              <td className={styles.LLabel}>거래일자</td>
               <td>
                 {salesResult?.sale_date ? format(new Date(salesResult.sale_date), 'yyyy년 MM월 dd일') : '날짜 없음'}
               </td>
@@ -255,223 +86,68 @@ const SalesPrintFormComponent: React.FC<any> = ({ salesFormData }) => {
         </table>
       </div>
 
-      <table css={infoTableStyle}>
+      <table className={styles.infoTable}>
         <thead>
           <tr>
-            <th colSpan={2} css={subTitleStyle}>
+            <th colSpan={2} className={styles.subTitle}>
               공급받는자 정보
             </th>
-            <th colSpan={2} css={subTitleStyle}>
+            <th colSpan={2} className={styles.subTitle}>
               공급자 정보
             </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td
-              css={css`
-                width: 15%;
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              상호
-            </td>
-            <td
-              css={css`
-                width: 35%;
-                text-align: left;
-              `}
-            >
-              {salesResult.client_name}
-            </td>
-            <td
-              css={css`
-                width: 15%;
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              등록번호
-            </td>
-            <td
-              css={css`
-                width: 35%;
-                text-align: center;
-              `}
-            >
+            <td className={styles.label}>상호</td>
+            <td className={styles.value}>{salesResult.client_name}</td>
+            <td className={styles.label}>등록번호</td>
+            <td className={styles.value}>
               {companyResult.business_number.replace(/(\d{3})(\d{2})(\d{5})/, '$1-$2-$3')}
             </td>
           </tr>
           <tr>
-            <td
-              css={css`
-                padding-right: 1rem;
-                font-weight: bold;
-              `}
-              rowSpan={2}
-            >
+            <td className={styles.label} rowSpan={2}>
               주소
             </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-              rowSpan={2}
-            >
+            <td className={styles.value} rowSpan={2}>
               {salesResult.client_address}
             </td>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              상호
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              {companyResult.company_name}
-            </td>
+            <td className={styles.label}>상호</td>
+            <td className={styles.value}>{companyResult.company_name}</td>
           </tr>
           <tr>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              대표자명
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              {companyResult.representative_name}
-            </td>
+            <td className={styles.label}>대표자명</td>
+            <td className={styles.value}>{companyResult.representative_name}</td>
           </tr>
           <tr>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              전화번호
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              {salesResult.client_tel}
-            </td>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              전화번호
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              {companyResult.tell_number}
-            </td>
+            <td className={styles.label}>전화번호</td>
+            <td className={styles.value}>{salesResult.client_tel}</td>
+            <td className={styles.label}>전화번호</td>
+            <td className={styles.value}>{companyResult.tell_number}</td>
           </tr>
           <tr>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              팩스번호
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              {salesResult.client_fax}
-            </td>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              팩스번호
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              {companyResult.fax_number}
-            </td>
+            <td className={styles.label}>팩스번호</td>
+            <td className={styles.value}>{salesResult.client_fax}</td>
+            <td className={styles.label}>팩스번호</td>
+            <td className={styles.value}>{companyResult.fax_number}</td>
           </tr>
           <tr>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              합계금액
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              ₩{total.toLocaleString()}
-            </td>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              주소
-            </td>
-            <td
-              css={css`
-                text-align: left;
-              `}
-            >
-              {companyResult.business_address}
-            </td>
+            <td className={styles.label}>합계금액</td>
+            <td className={styles.value}>₩{total.toLocaleString()}</td>
+            <td className={styles.label}>주소</td>
+            <td className={styles.value}>{companyResult.business_address}</td>
           </tr>
           <tr>
-            <td
-              css={css`
-                font-weight: bold;
-                text-align: center;
-              `}
-            >
-              적요
-            </td>
-            <td
-              colSpan={3}
-              css={css`
-                text-align: left;
-              `}
-            >
+            <td className={styles.label}>적요</td>
+            <td colSpan={3} className={styles.value}>
               {salesResult.description}
             </td>
           </tr>
         </tbody>
       </table>
 
-      <table css={transactionTableStyle}>
+      <table className={styles.transactionTable}>
         <thead>
           <tr>
             <th>번호</th>
@@ -485,7 +161,7 @@ const SalesPrintFormComponent: React.FC<any> = ({ salesFormData }) => {
         </thead>
         <tbody>
           {salesItemsResult.map((item, index) => (
-            <tr key={item.sales_item_id}>
+            <tr key={item.sales_item_id.toString()}>
               <td>{index + 1}</td>
               <td>{`${item.product_name}${item.standard ? `[${item.standard}]` : ''}`}</td>
               <td>{`${item.quantity.toLocaleString()}${item.unit ? `[${item.unit}]` : ''}`}</td>
@@ -498,175 +174,41 @@ const SalesPrintFormComponent: React.FC<any> = ({ salesFormData }) => {
         </tbody>
         <tfoot>
           <tr>
-            <td
-              colSpan={2}
-              css={css`
-                font-weight: bold;
-                background-color: #f9f9f9;
-                text-align: right;
-                border-top: 2px solid black;
-              `}
-            >
+            <td colSpan={2} className={styles.footerLabel}>
               합계
             </td>
-            <td css={smallTextStyle}>{totalQuantity.toLocaleString()}</td>
-            <td css={smallTextStyle}>₩{totalP.toLocaleString()}</td>
-            <td css={smallTextStyle}>₩{totalPrice.toLocaleString()}</td>
-            <td css={smallTextStyle}>₩{totalSub_price.toLocaleString()}</td>
-            <td css={smallTextStyle}></td>
+            <td className={styles.footerValue}>{totalQuantity.toLocaleString()}</td>
+            <td className={styles.footerValue}>₩{totalP.toLocaleString()}</td>
+            <td className={styles.footerValue}>₩{totalPrice.toLocaleString()}</td>
+            <td className={styles.footerValue}>₩{totalSub_price.toLocaleString()}</td>
+            <td className={styles.footerValue}></td>
           </tr>
         </tfoot>
       </table>
 
-      <table css={[transactionTableStyle, resultTransactionTableStyle]}>
+      <table className={`${styles.transactionTable} ${styles.resultTransactionTable}`}>
         <tbody>
           <tr>
-            <td
-              css={css`
-                background-color: #f0f0f0;
-                font-weight: bold;
-                text-align: center;
-                width: 8%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            >
-              공급가액
-            </td>
-            <td
-              css={css`
-                background-color: #ffffff;
-                font-weight: bold;
-                text-align: center;
-                width: 12%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            >
-              ₩{totalPrice.toLocaleString()}
-            </td>
-            <td
-              css={css`
-                background-color: #f0f0f0;
-                font-weight: bold;
-                text-align: center;
-                width: 8%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            >
-              세액
-            </td>
-            <td
-              css={css`
-                background-color: #ffffff;
-                font-weight: bold;
-                text-align: center;
-                width: 12%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            >
-              ₩{totalSub_price.toLocaleString()}
-            </td>
-            <td
-              css={css`
-                background-color: #f0f0f0;
-                font-weight: bold;
-                text-align: center;
-                width: 8%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            >
-              합계금액
-            </td>
-            <td
-              css={css`
-                background-color: #ffffff;
-                font-weight: bold;
-                text-align: center;
-                width: 12%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            >
-              ₩{total.toLocaleString()}
-            </td>
-            <td
-              css={css`
-                background-color: #f0f0f0;
-                font-weight: bold;
-                text-align: center;
-                width: 8%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            >
-              인수자
-            </td>
-            <td
-              css={css`
-                background-color: #ffffff;
-                font-weight: bold;
-                text-align: center;
-                width: 12%;
-                margin-left: 0;
-                margin-right: 0;
-                padding-left: 0;
-                padding-right: 0;
-                font-size: x-small;
-                white-space: nowrap;
-              `}
-            ></td>
+            <td className='header'>공급가액</td>
+            <td className='value'>₩{totalPrice.toLocaleString()}</td>
+            <td className='header'>세액</td>
+            <td className='value'>₩{totalSub_price.toLocaleString()}</td>
+            <td className='header'>합계금액</td>
+            <td className='value'>₩{total.toLocaleString()}</td>
+            <td className='header'>인수자</td>
+            <td className='value'></td>
           </tr>
         </tbody>
       </table>
 
-      <table css={[transactionTableStyle, resultTransactionTableStyle]}>
+      <table className={`${styles.transactionTable} ${styles.resultTransactionTable}`}>
         <tbody>
           <tr>
-            <td css={accountNumberStyle}>계좌번호</td>
-            <td
-              css={css`
-                text-align: right;
-              `}
-            >
-              {companyResult.account}
-            </td>
+            <td className='header'>계좌번호</td>
+            <td className='value'>{companyResult.account}</td>
           </tr>
         </tbody>
       </table>
     </div>
   );
-};
-
-export default SalesPrintFormComponent;
+}
--- util/fetchSalesData.ts
+++ util/fetchSalesData.ts
@@ -1,6 +1,11 @@
 'use server';
 
-import { SalesPrintFormData } from '@/components/(회계)/(판매)/(Print)/SalesPrintForm';
+import {
+  CompanyResult,
+  SalesItemResult,
+  SalesPrintFormData,
+  SalesResult,
+} from '@/components/(회계)/(판매)/(Print)/SalesPrintForm';
 import { executeQuery } from '@/lib/db';
 import { getTokenUserData } from '@/util/token';
 import { ACT } from 'auth';
@@ -16,17 +21,38 @@ export async function fetchSalesData(id: string): Promise<SalesPrintFormData> {
     executeQuery('SELECT * FROM sales_items WHERE sales_id = ?', [salesIdBuffer]),
   ]);
   return {
-    companyResult: transformBufferFields(companyResult[0]),
-    salesResult: transformBufferFields(salesResult[0]),
-    salesItemsResult: salesItemsResult.map((item) => transformBufferFields(item)),
+    companyResult: companyResult[0],
+    salesResult: salesResult[0],
+    salesItemsResult,
   };
 }
 
-// Buffer 필드를 hex 문자열로 변환하는 함수
-function transformBufferFields(data: Record<string, any>): Record<string, any> {
+export async function fetchSalesData2(id: string): Promise<SalesPrintFormData> {
+  const data = (await getTokenUserData()) as ACT;
+  const companyIdBuffer = Buffer.from(data.companyId['data'], 'hex');
+  const salesIdBuffer = Buffer.from(id, 'hex');
+
+  const [companyResult, salesResult, salesItemsResult] = await Promise.all([
+    executeQuery('SELECT * FROM company WHERE company_id = ?', [companyIdBuffer]),
+    executeQuery('SELECT * FROM sales WHERE sales_id = ?', [salesIdBuffer]),
+    executeQuery('SELECT * FROM sales_items WHERE sales_id = ?', [salesIdBuffer]),
+  ]);
+
+  return {
+    companyResult: transformBufferFields<CompanyResult>(companyResult[0]),
+    salesResult: transformBufferFields<SalesResult>(salesResult[0]),
+    salesItemsResult: salesItemsResult.map((item) => transformBufferFields<SalesItemResult>(item)),
+  };
+}
+
+function transformBufferFields<T>(data: Record<string, any>): T {
   const transformedData: Record<string, any> = {};
   for (const key in data) {
     transformedData[key] = Buffer.isBuffer(data[key]) ? data[key].toString('hex') : data[key];
   }
-  return transformedData;
+  return transformedData as T;
+}
+
+export async function generateSalesPrintHtml(id: string): Promise<string> {
+  return 'html';
 }
