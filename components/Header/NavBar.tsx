5fafaf7e754b5916e030bb8ba38ff12e0160c0a5
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-11-12 00:45:42 +0900

    왜이리 깜빡이나 했네

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -1,18 +1,17 @@
-import { getLoggedin } from '@/util/getLoggedin';
 import AS from './AfterSignIn';
-import BS from './BeforeSignIn';
 import HomeLink from './HomeLink';
 import styles from './NavBar.module.css';
 
-export default async function NavBar() {
-  const isLoggedIn = await getLoggedin();
-
+export default function NavBar() {
   return (
     <div className={styles.navWrapper}>
       <nav className={styles.nav}>
         <HomeLink />
 
-        <div className={styles.rightLinks}>{isLoggedIn ? <AS /> : <BS />}</div>
+        <div className={styles.rightLinks}>
+          {' '}
+          <AS />
+        </div>
       </nav>
     </div>
   );
